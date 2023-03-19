6a6d11807549f2f772e05e38f742b0df1a4e6033
Elias Neto <eliasneto321@outlook.com>
2023-03-19 18:08:02 -0300

    responsivity aplicated

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const buttonMenu = document.querySelector(".menu-button")
 const nav = document.querySelector("nav")
+const iconOpenMenu = document.querySelector(".menu-button .icon-openMenu")
+const iconCloseMenu = document.querySelector(".menu-button .icon-closeMenu")
 
 function handleMenu() {
   nav.classList.toggle("open")
+  iconCloseMenu.classList.toggle("hidden")
+  iconOpenMenu.classList.toggle("hidden")
 }
 
 buttonMenu.addEventListener("click", handleMenu)
